474f3d1eac913984702a4120663eed588ec29e29
andre classen <aclassen@hotmail.de>
2018-07-02 20:15:06 +0200

    SFW-187 Updated debug tracker broadcast format

--- loggingtrackingprovider/src/main/java/io/stanwood/framework/analytics/debug/DebugViewTrackerImpl.java
+++ loggingtrackingprovider/src/main/java/io/stanwood/framework/analytics/debug/DebugViewTrackerImpl.java
@@ -5,9 +5,11 @@ import android.app.Application;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
+import android.util.Log;
 
-import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Map;
 
 import io.stanwood.framework.analytics.generic.TrackerParams;
 
@@ -24,25 +26,41 @@ public class DebugViewTrackerImpl extends DebugViewTracker {
     @Override
     public void track(@NonNull TrackerParams params) {
         JSONObject object = new JSONObject();
-        try {
-            object.put("time", System.currentTimeMillis());
-            object.put("eventname", params.getEventName());
-            if (params.getCustomPropertys() != null) {
-                object.put("itemid", params.getCustomPropertys());
-            } else if (!TextUtils.isEmpty(params.getItemId())) {
-                object.put("itemid", params.getItemId());
-            }
-            if (!TextUtils.isEmpty(params.getName())) {
-                object.put("name", params.getName());
-            }
-            Intent intent = new Intent("io.stanwood.action.log.tracker");
-            intent.putExtra("data", object.toString());
-            intent.putExtra("appid", context.getPackageName());
-            context.sendBroadcast(intent);
-
-        } catch (JSONException e) {
-            // noop
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.currentTimeMillis())
+                .append("|")
+                .append(params.getEventName())
+                .append("|");
+        if (!TextUtils.isEmpty(params.getItemId())) {
+            sb.append("itemid").append("|").append(params.getItemId()).append("|");
         }
+        if (!TextUtils.isEmpty(params.getName())) {
+            sb.append("name").append("|").append(params.getName()).append("|");
+        }
+        if (!TextUtils.isEmpty(params.getContentType())) {
+            sb.append("content type").append("|").append(params.getContentType()).append("|");
+        }
+        if (!TextUtils.isEmpty(params.getCategory())) {
+            sb.append("category").append("|").append(params.getCategory()).append("|");
+        }
+        if (params.getCustomPropertys() != null && !params.getCustomPropertys().isEmpty()) {
+            for (Map.Entry<String, Object> entry : params.getCustomPropertys().entrySet()) {
+                sb.append(entry.getKey()).append("|").append(entry.getValue().toString()).append("|");
+            }
+        }
+        sb.setLength(sb.length() - 1);
+        Intent intent = new Intent("io.stanwood.debugapp.plugin");
+        intent.putExtra("source", "debugtracker");
+        intent.putExtra("data", sb.toString());
+        context.sendBroadcast(intent);
+    }
+
+    @Override
+    public void track(@NonNull Throwable throwable) {
+        Intent intent = new Intent("io.stanwood.debugapp.plugin");
+        intent.putExtra("source", "debugtracker");
+        intent.putExtra("data", System.currentTimeMillis() + "|exception|" + Log.getStackTraceString(throwable));
+        context.sendBroadcast(intent);
     }
 
     public static class Builder extends DebugViewTracker.Builder {
